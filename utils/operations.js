86ff3e0ab6855b38181990d073d7b5ed60f504b8
Fajer <s202280160@kfupm.edu.sa>
2025-11-05 11:37:10 +0300

    todo4

--- utils/operations.js
+++ utils/operations.js
@@ -1,16 +1,22 @@
 export function add(numbers) {
-  return {/*write code here*/}
+  return numbers.reduce((sum, num) => sum + num, 0);
 }
 
 export function subtract(numbers) {
-  return {/*write code here*/}
+    return numbers.reduce((diff, num) => diff - num);
 }
 
 export function multiply(numbers) {
-  return {/*write code here*/}
+  return numbers.reduce((product, num) => product * num, 1);
 }
 
 export function divide(numbers) {
-  return {/*write code here*/}
+  return numbers.reduce((quotient, num) => {
+      if (num === 0) {
+          console.log("Error: Division by zero");
+          process.exit(1);
+      }
+      return quotient / num;
+  });
 }
 
